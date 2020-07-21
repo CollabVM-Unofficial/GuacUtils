343b2a89b2827b56105795b5676bc91a32c62218
linuks <linuks777@protonmail.com>
2020-07-21 14:40:01 -0400

    formatting corrections

--- guac_utils.hpp
+++ guac_utils.hpp
@@ -8,24 +8,32 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> guac_decode(std::string encoded_str) {
-    // ported from totallyNotABot
-    std::vector<std::string> ret;
+std::vector<std::string> guac_decode(std::string encoded_str)
+{
+	// ported from totallyNotABot
+	std::vector<std::string> ret;
 	std::string _msg = msg;
 
 	for (int i = 0; i < _msg.length();)
 	{
 		// Decode the length part
 		int lenEnd = _msg.find('.');
-		if (lenEnd == std::string::npos) break;
+		if (lenEnd == std::string::npos)
+			break;
 		int len = std::stoi(_msg.substr(i, lenEnd));
 		// Decode the text part
 		i += lenEnd + 1; // Put i after the .
 		std::string text = _msg.substr(i, len);
 		ret.push_back(text);
 		i += len; // Put i on the ,/;
-		if (_msg[i] == ';') { break; }
-		else if (_msg[i] == ',') { i++; }
+		if (_msg[i] == ';')
+		{
+			break;
+		}
+		else if (_msg[i] == ',')
+		{
+			i++;
+		}
 		//else throw new FormatException($"Expexcted ',' or ';', but got {_msg[i]}");
 		_msg = _msg.substr(i);
 		i = 0;
@@ -33,14 +41,16 @@ std::vector<std::string> guac_decode(std::string encoded_str) {
 	return ret;
 }
 
-std::string guac_encode(std::vector<std::string> not_encoded) {
-    std::string ret;
-    for (const std::string &part : not_encoded) {
-        ret.append(std::to_string(part.length())).append(".").append(part).append(","); // appends <length>.<part>,
-    }
-    ret.erase(ret.length()-1); // erase the last ,
-    ret.append(";"); // finish the guac message with ;
-    return ret;
+std::string guac_encode(std::vector<std::string> not_encoded)
+{
+	std::string ret;
+	for (const std::string &part : not_encoded)
+	{
+		ret.append(std::to_string(part.length())).append(".").append(part).append(","); // appends <length>.<part>,
+	}
+	ret.erase(ret.length() - 1); // erase the last ,
+	ret.append(";");			 // finish the guac message with ;
+	return ret;
 }
 
 #endif // __GUAC_UTILS_HPP__
