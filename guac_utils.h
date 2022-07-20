399a8913a91857163596db59066f079a703b2898
Paper <37962225+mrpapersonic@users.noreply.github.com>
2022-07-20 00:21:37 -0400

    Remove the need to have a temp variable
    also there shouldn't be any more segtraps and shit

--- guac_utils.h
+++ guac_utils.h
@@ -11,42 +11,38 @@
  * Decodes a guac-encoded string.
  * this function is not safe!!! make sure your strings are terminated ;)
  * my first time dealing with dynamic memory management...
- * stuff can and WILL break
  *
  * char* encoded_str | C-style encoded string to decode
  * char** ret        | malloc'd (empty) char pointer array
  * int* reta         | pointer to an int, stores the amount of items in the array
 **/
 char** guac_decode(char* encoded_str, char** ret, int* reta) {
+	char* _msg = calloc(strlen(encoded_str) + 1, sizeof(char));
+	if (_msg == NULL)
+		return NULL;
+	strncpy(_msg, encoded_str, strlen(encoded_str));
 	int i, j;
-	for (j = i = 0; i < strlen(encoded_str); j++) {
-		char* tmp = calloc(1, sizeof(char));
+	for (j = i = 0; i < strlen(_msg); j++) {
 		/* length */
-		int len_end = (strchr(encoded_str, '.') - encoded_str);
-		if (encoded_str[len_end] == '\0') {
+		int len_end = (strchr(_msg, '.') - _msg);
+		if (_msg[len_end] == '\0') {
 			break;
 		}
-		tmp = realloc(tmp, len_end * sizeof(char));
-		memcpy(tmp, &encoded_str[i], len_end);
-		tmp[len_end] = '\0';
-		int len = atoi(tmp);
-		tmp[0] = '\0'; // nobody will notice i'm wearing the same shirt
+		int len = atoi(strtok(_msg, "."));
 		/* data */
 		i += (len_end + 1);
-		tmp = realloc(tmp, len * sizeof(char));
-		memcpy(tmp, &encoded_str[i], len);
-		i += len;
-		tmp[len] = '\0';
-		ret = realloc(ret, (j + 1) * sizeof(char*));
-		ret[j] = calloc(len, sizeof(char));
-		strncpy(ret[j], tmp, len);
-		if (encoded_str[i] == ',') {
+		if (_msg[i] == ',') {
 			i++;
-		} else if (encoded_str[i] == ';') {
+		} else if (_msg[i] == ';') {
 			break;
 		}
-		free(tmp);
+		ret = realloc(ret, (j + 1) * sizeof(char*));
+		ret[j] = calloc(len, sizeof(char));
+		strncpy(ret[j], &_msg[i], len);
+		strncpy(_msg, &_msg[i+len+1], strlen(encoded_str));
+		i = 0;
 	}
+	free(_msg);
 	*reta = j;
 	return ret;
 }
@@ -59,10 +55,11 @@ char** guac_decode(char* encoded_str, char** ret, int* reta) {
  * int reta   | amount of items in list, returned by guac_decode
 **/
 char* guac_encode(char** ret, int reta) {
+	int i;
 	char* str = calloc(1, sizeof(char));
-	for (int i = 0; i <= reta; i++) {
-		str = realloc(str, (3 + (strlen(ret[i]))) * sizeof(char));
-		sprintf(str + strlen(str), "%d.%s,", strlen(ret[i]), ret[i]);
+	for (i = 0; i < reta; i++) {
+		str = realloc(str, sizeof(str) + ((strlen(ret[i])) * sizeof(char)));
+		sprintf(str + strlen(str), "%lld.%s,", strlen(ret[i]), ret[i]);
 	}
 	str[strlen(str)-1] = ';';
 	return str;
